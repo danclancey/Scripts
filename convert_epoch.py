5f90decef2b10be18c7b5d6c7954a5ab594202ea
danclancey <144161647+danclancey@users.noreply.github.com>
2023-09-23 23:00:34 -0400

    Update convert_epoch.py

--- convert_epoch.py
+++ convert_epoch.py
@@ -1,3 +1,31 @@
+"""
+---------------------------------------------------------------
+Script Name: Epoch Time Converter
+Author: Dan
+Date: 7-May-2023
+Version: 1.0
+Description:
+    This script converts a given epoch time to a human-readable datetime string formatted as "YYYY-MM-DD_HHMM",
+    adjusting for the US Eastern timezone.
+
+Usage:
+    python epoch_time_converter.py [epoch_time]
+
+    Where:
+    [epoch_time] is the epoch time to convert, specified as an integer.
+
+Examples:
+    python epoch_time_converter.py 1619710738000
+    Output: 2023-04-29_0806PM
+
+Requirements:
+    - Python 3.x
+    - argparse library
+    - datetime library
+    - pytz library
+---------------------------------------------------------------
+"""
+
 import argparse
 import datetime
 import pytz
